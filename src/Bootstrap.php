d6a21cb397efb3ef4141cd94de91a16e3057635e
Lucas Bickel <lucas.bickel@swisscom.com>
2013-07-23 20:15:30 +0200

    basic cli scaffolding

---
+++ src/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * bootstrap
+ *
+ * @category  Application
+ * @package   CommentFinder
+ * @author    Lucas S. Bickel <lucas.bickel@swisscom.com>
+ * @copyright 2013 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://www.swisscom.com
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
---
+++ test/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * test bootstrap
+ *
+ * @category  Application
+ * @package   CommentFinder
+ * @author    Lucas S. Bickel <lucas.bickel@swisscom.com>
+ * @copyright 2013 - Alle Rechte vorbehalten
+ * @license   GPL http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link      http://www.swisscom.com
+ */
+
+require_once __DIR__.'/../src/Bootstrap.php';
